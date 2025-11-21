9307cac346279df542a164e9aa6b8beedde1f3ae
Ren0g <98395283+Ren0g@users.noreply.github.com>
2025-11-21 23:20:43 +0100

    Update page.tsx

--- app/mladji/page.tsx
+++ app/mladji/page.tsx
@@ -1,6 +1,6 @@
 import LeagueView from "@/components/LeagueView";
 
-export default function MladjiPage() {
+export default function MladjiPioniriPage() {
   return (
     <div className="p-4">
       <LeagueView leagueCode="MLADJI" />
